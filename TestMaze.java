fd575c88bebbb16ab34a73a10e826b2d277b850e
Leo is awesome <lchan1994@yahoo.com>
2015-05-29 19:23:54 -0400

    it compiles

--- TestMaze.java
+++ TestMaze.java
@@ -1,5 +1,121 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.awt.Point;
+
 public class TestMaze {
+	private static final char TRAVELER = 'x';
+	private static final char OBSTACLE = 'O';
+	private static final char GROUND = '.';
+	private static final char GOAL = '$';
+
 	public static void main(String[] args){
-		
+		Scanner scan = new Scanner(System.in);
+		ArrayList<String> lines = new ArrayList<>();
+		while (scan.hasNextLine()){
+			lines.add(scan.nextLine());
+		}
+		char[][] map = new char[lines.size()][lines.get(0).length()];
+		int initX = 0, initY = 0;
+		for (int y = 0; y < map.length; y++){
+			for (int x = 0; x < map[0].length; x++){
+				char c = lines.get(y).charAt(x);
+				map[y][x] = c;
+				if (c == TRAVELER){
+					initX = x;
+					initY = y;
+				}
+			}
+		}
+
+		Traveler t = new Traveler(initX, initY);
+		while (!travelerDidReachGoal(t, map) && t.canReachGoal()){ // each iteration represents 1 turn
+			int x = (int)t.getPos().getX();
+			int y = (int)t.getPos().getY();
+
+			// Check for a certain condition to see if the traveler can move.
+			// Let the example position be if the traveler can move another space
+			boolean upCond = false, rightCond = false, downCond = false, leftCond = true;
+			if (y > 0){
+				if (map[y-1][x] != OBSTACLE){
+					upCond = true;
+				}
+			}
+			if (y < map.length-1){
+				if (map[y+1][x] != OBSTACLE){
+					downCond = true;
+				}
+			}
+			if (x > 0){
+				if (map[y][x-1] != OBSTACLE){
+					leftCond = true;
+				}
+			}
+			if (x < map[0].length-1){
+				if (map[y][x+1] != OBSTACLE){
+					rightCond = true;
+				}
+			}
+			Direction[] possibleDirections = t.getPossibleDirections(map[0].length, map.length, upCond, rightCond, downCond, leftCond); // all possible directions the traveler can move
+
+			// Some condition is met and the traveler can continue move.
+			// In this case, the condition is if the traveler can move in a direction.
+			if (possibleDirections.length > 0){
+				t.moveTo(possibleDirections[0]); // from a list of possible directions
+			}
+			else {
+				// The traveler cannot move and must retrace his steps.
+				// He will need to move back and remember not to move in the previous direction he made.
+				t.moveBack();
+			}
+		}
+
+		int testX = initX, testY = initY;
+		if (travelerDidReachGoal(t, map)){
+			Direction[] path = t.getPath();
+			for (Direction d : path){
+				System.out.println(d);
+				switch (d){
+					case UP:
+						printMap(new Point(initX, initY), new Point(testX, --testY), map);
+						break;
+					case RIGHT:
+						printMap(new Point(initX, initY), new Point(++testX, testY), map);
+						break;
+					case DOWN:
+						printMap(new Point(initX, initY), new Point(testX, ++testY), map);
+						break;
+					case LEFT:
+						printMap(new Point(initX, initY), new Point(--testX, testY), map);
+						break;
+				}
+			}
+		}
+		else {
+			System.out.println("Could not find a path");
+		}
+	}
+
+	private static boolean travelerDidReachGoal(Traveler t, char[][] map){
+		int x = (int)t.getPos().getX();
+		int y = (int)t.getPos().getY();
+		return map[y][x] == GOAL;
+	}
+
+	private static void printMap(Point oldPos, Point newPos, char[][] map){
+		for (int y = 0; y < map.length; y++){
+			for (int x = 0; x < map[0].length; x++){
+				if ((int)oldPos.getX() == x && (int)oldPos.getY() == y){
+					System.out.print(GROUND);
+				}
+				else if ((int)newPos.getX() == x && (int)newPos.getY() == y){
+					System.out.print(TRAVELER);
+				}
+				else {
+					System.out.print(map[y][x]);
+				}
+			}
+			System.out.println("");
+		}
+		System.out.println("");
 	}
 }
--- Traveler.java
+++ Traveler.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.awt.Point;
 
 /**
  * This class will traverse the maze on each turn, remembering where he went.
@@ -6,7 +7,12 @@ import java.util.ArrayList;
  * be the successful path taken from the starting to end point.
  */
 public class Traveler {
-	private int x,y;
+	private Point pos;
+
+	/**
+	 * Keep track of the places you have been and do not go there.
+	 */
+	private ArrayList<Point> previousPositions = new ArrayList<>();
 
 	/**
 	 * A list of (bad) directions the traveler made in the previous turns.
@@ -32,8 +38,7 @@ public class Traveler {
 	private boolean reachableGoal = true;
 
 	public Traveler(int x, int y){
-		this.x = x;
-		this.y = y;
+		this.pos = new Point(x,y);
 	}
 
 	/**
@@ -42,23 +47,25 @@ public class Traveler {
 	 */
 	public void moveTo(Direction d){
 		// Update the traveler's position.
+		int x = (int)this.pos.getX(), y = (int)this.pos.getY();
 		switch (d){
 			case UP:
-				this.y--;
+				this.pos.move(x, y-1);
 				break;
 			case RIGHT:
-				this.x++;
+				this.pos.move(x+1, y);
 				break;
 			case DOWN:
-				this.y++;
+				this.pos.move(x, y+1);
 				break;
 			case LEFT:
-				this.x--;
+				this.pos.move(x-1, y);
 				break;
 		}
 
 		// Remember this position in case the traveler is unable to move in the next turn.
 		this.previousDirections.add(d);
+		this.previousPositions.add(new Point(x,y));
 
 		// If the traveler just moved to this square, he does not know of any bad directions
 		// and makes a new (empty) list of bad directions.
@@ -79,27 +86,137 @@ public class Traveler {
 			// the list of previous directions (by either 1 or 2, though if it is
 			// 2, that means the traveler has just moved back and will be moving back
 			// once more.)
-			this.previousDirections.remove(this.previousDirections.size()-1);
+			Direction removedDirection = this.previousDirections.remove(this.previousDirections.size()-1);
 
 			// The list of bad directions should always be at most 1 greater than that
 			// of the list of previous directions. If it is greater, cut off the last nested list
 			// of bad directions from the parent list, badDirections.
 			if (this.badDirections.size() - this.previousDirections.size() > 1){
-				this.badDirections.removeLast();
+				this.badDirections.remove(this.badDirections.size()-1);
 			}
 
 			// Add the last direction made to the list of the latest bad directions.
-			this.badDirections.last().add(this.lastDirection);
+			this.badDirections.get(this.badDirections.size()-1).add(removedDirection);
 
-			// Update the position.
-			this.x = old x;
-			this.y = old y;
+			// Update the position and delete the last pos.
+			this.pos.setLocation( this.previousPositions.remove(this.previousPositions.size()-1) );
 		}
 		else{
 			// The traveler was either forced back to the starting point and cannot move further
 			// or the traveler could not move at all to start with. In this case, the goal is not
 			// reachable.
-			this.canReachGoal = false;
+			this.reachableGoal = false;
 		}
+	}
+
+	/**
+	 * Return all possible directions the traveler can move. This excludes
+	 * all the ones in badDirections and the latest previousDirection. Also
+	 * exclude a direction if it leads the traveler to a square he has previously
+	 * been on.
+	 * 
+	 * This is checked BEFORE the traveler makes his move.
+	 * @param mapWidth  Traveler cannot go past the map width.
+	 * @param mapHeight Traveler cannot go past the map height.
+	 * @param mapConditions Any optional conditions set by the map regarding certain directions.
+	 *                      [up condition, right condition, down condition, left condition]
+	 * @return all that
+	 */
+	public Direction[] getPossibleDirections(int mapWidth, int mapHeight, boolean... mapConditions){
+		ArrayList<Direction> possibleDirections = new ArrayList<>();
+		int x = (int)this.pos.getX(), y = (int)this.pos.getY();
+		ArrayList<Direction> latestBadDirections = ( this.badDirections.size() > 0 ? this.badDirections.get(this.badDirections.size()-1) : new ArrayList<Direction>() );
+		Direction latestDirection = ( this.previousPositions.size() > 0 ? this.previousDirections.get(this.previousDirections.size()-1) : null );
+
+		boolean upCond = (mapConditions.length > 0 ? mapConditions[0] : true);
+		boolean rightCond = (mapConditions.length > 1 ? mapConditions[1] : true);
+		boolean downCond = (mapConditions.length > 2 ? mapConditions[2] : true);
+		boolean leftCond = (mapConditions.length > 3 ? mapConditions[3] : true);
+
+		// UP
+		if (y > 0 &&
+			!latestBadDirections.contains(Direction.UP) &&
+			latestDirection != getOppositeDirection(Direction.UP) &&
+			!this.wasOnPoint(x, y-1) &&
+			upCond){
+			possibleDirections.add(Direction.UP);
+		}
+
+		// DOWN
+		if (y < mapHeight-1 &&
+			!latestBadDirections.contains(Direction.DOWN) &&
+			latestDirection != getOppositeDirection(Direction.DOWN) &&
+			!this.wasOnPoint(x, y+1) &&
+			downCond){
+			possibleDirections.add(Direction.DOWN);
+		}
+
+		// LEFT
+		if (x > 0 &&
+			!latestBadDirections.contains(Direction.LEFT) &&
+			latestDirection != getOppositeDirection(Direction.LEFT) &&
+			!this.wasOnPoint(x-1, y) &&
+			leftCond){
+			possibleDirections.add(Direction.LEFT);
+		}
+
+		// RIGHT
+		if (x < mapWidth-1 &&
+			!latestBadDirections.contains(Direction.RIGHT) &&
+			latestDirection != getOppositeDirection(Direction.RIGHT) &&
+			!this.wasOnPoint(x+1, y) &&
+			rightCond){
+			possibleDirections.add(Direction.RIGHT);
+		}
+
+		return possibleDirections.toArray(new Direction[possibleDirections.size()]);
+	}
+
+	/**
+	 * Check to see if the destination was already reached
+	 * @param  x coord
+	 * @param  y coord
+	 * @return   did you go on it?
+	 */
+	private boolean wasOnPoint(int x, int y){
+		return previousPositions.contains(new Point(x, y));
+	}
+
+	/**
+	 * Returns the opposite direction of a given one
+	 * @param  d ank
+	 * @return   memes yo
+	 */
+	private static Direction getOppositeDirection(Direction d){
+		switch (d){
+			case UP: return Direction.DOWN;
+			case RIGHT: return Direction.LEFT;
+			case DOWN: return Direction.UP;
+			case LEFT: return Direction.RIGHT;
+		}
+		return null; // Shouldn't be able to reach this
+	}
+
+	/**
+	 * Check if the traveler can even make it to his goal.
+	 * @return reachableGoal
+	 */
+	public boolean canReachGoal(){
+		return this.reachableGoal;
+	}
+
+	/**
+	 * Return the current position
+	 */
+	public Point getPos(){
+		return this.pos;
+	}
+
+	/**
+	 * After traversing the map, get the path of the traveler.
+	 * @return previousDirections
+	 */
+	public Direction[] getPath(){
+		return this.previousDirections.toArray(new Direction[this.previousDirections.size()]);
 	}
 }
